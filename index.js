a7ac02dac2818b3ac8c3ea7399151897e6e5f3bc
Abhay <abhaykvincent@gmail.com>
2021-08-27 17:53:47 -0400

    done ✅

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 maxArea = function(height) {
+    let areas=[];
     for (var i = 0; i < height.length; i++) {
-        console.log(height[i])
+        areas.push(0)
+        for (var j = i; j < height.length; j++) {
+            let min = Math.min(height[i], height[j]);
+            let area=min*(j-i)
+            if(area>areas[i])
+                areas[i]=area
+        }
     }
+    console.log(Math.max(...areas))
+    return parseInt(Math.max(areas)) 
 };
-maxArea([1,3,2,3])
+maxArea([1,8,6,2,5,4,8,3,7])
